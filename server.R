57319fbfd3de16f48d3c16071f46fbf42c800ec4
Siqi1234 <smao2@uw.edu>
2022-03-06 16:08:36 -0800

    merge changes

--- server.R
+++ server.R
@@ -8,19 +8,41 @@
 #
 
 library(shiny)
+library("ggplot2")
+library("tidyverse")
 
 # Define server logic required to draw a histogram
-shinyServer(function(input, output) {
-
-    output$distPlot <- renderPlot({
-
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-
-    })
-
+my_server <- function(input, output, session){
+  # Wrangle Data for chart_2 Input
+  
+  uspollution <- read.csv("data/uspollution_pollution_us_2010_2016.csv", stringsAsFactors = FALSE)
+  uspollution <-  mutate(uspollution, year = c(str_sub(uspollution$Date.Local, 1, 4)))
+  uspollution <- mutate(
+    uspollution,
+    state_county = paste(County, State, sep = ",")
+  )
+  
+  counties_ave <- uspollution %>%
+    group_by(year, State, County, SO2.Units) %>%
+    summarise(ave_so2_aqi = mean(SO2.AQI, na.rm = TRUE))
+  
+  Ohio_situation <- counties_ave %>%
+    filter(State == "Ohio")
+  
+  output$second_chart_ggplot <- renderPlot({
+    req(input$select_county)
+    if (identical(input$select_county, "")) return(NULL)
+    ggplot(data = filter(Ohio_situation, County %in% input$select_county)) +
+      geom_point(aes(x = year, y = ave_so2_aqi, color = County %in% input$select_county)) +
+      guides(color = guide_legend(title = "County"))+
+      scale_color_discrete(labels = input$select_county)+
+      labs(
+        x = "Year",
+        y = "Average SO2 AQI",
+        title = "Average SO2 AQI in Ohio(2010-2016)")
+    
+    
+    
 })
+    
+}
--- ui.R
+++ ui.R
@@ -13,11 +13,54 @@ page_one <- tabPanel(
   "Introduction", 
   tab_introduction_panel)
 
+
+#Create a siderbar for second chart panel
+second_chart_siderbar <- sidebarPanel(
+  selectizeInput(
+    inputId = "select_county",
+    label = "Select a county:",
+    choices = c("Cuyahoga", "Hamilton", "Medina"),
+    selected = "Cuyahoga")
+)
+
+#Create a main panel for second chart panel
+
+second_chart_main <- mainPanel(
+  h3("Average SO2 AQI in Ohio(2010-2016)"),
+  plotOutput(
+    outputId = "second_chart_ggplot"
+  )
+)
+
+second_chart_panel <- tabPanel(
+  "Second Chart",
+  tabsetPanel(
+    type = "tabs",
+    tabPanel("Plot",
+  second_chart_siderbar,
+  second_chart_main
+),
+tabPanel(
+  "Summary",
+  h1("Summary"),
+  p("In the second chart we noticed that Ohio has the highest parts per billion. 
+    This chart focuses on Ohio plotting from 2010-2016 focusing on the pollution 
+    in Ohio's specific counties. We notice in 2011 it is at an all time high and 
+    by 2016 we can already see some changes. In 2011 we can see that there 
+    is over 20 parts per billion for Cuyahoga and Hamilton (We notice 
+    Medina disappears after 2010). In 2016 we visibly notice changes in the 
+    scatter plot showing a major decrease in amounts of average SO2 AQI in 
+    Cuyahoga and Hamilton to the range of 0-10 parts per billion. ")
+)
+),
+)
+
 # Pass each page to a multi-page layout 
 ui <- navbarPage(
   "US Pollution",
   theme = shinytheme("cyborg"),
   page_one,
+  second_chart_panel,
   includeCSS("style.css")
  )
 
